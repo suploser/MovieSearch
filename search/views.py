5160fbc84cb18ef30be367a040cdb46f471855a1
suploser <sup_loser@outlook.com>
2018-05-20 21:24:18 +0800

    视图

--- search/views.py
+++ search/views.py
@@ -29,7 +29,7 @@ def suggest(request):
         )
     title_list = []
     for response in response['suggest']['movie_suggest'][0]['options']:
-        title_list.append(response['_source']['title'])
+        title_list.append(response['_source']['title'][:20])
     return JsonResponse({'suggest':title_list})
 
 def search(request): 
@@ -41,13 +41,15 @@ def search(request):
         search_word = request.GET.get('search_word')
         if len(search_word)<1:
             raise Exception('请输入关键词')
+        if len(search_word)>20:
+            raise Exception('关键词过长')
         response = client.search(
                 index="movie",
                 body={
                     "query":{
                         "multi_match":{
                             "query":search_word,
-                            "fields":["title^3", "type", "introduction"]
+                            "fields":["title^3", "type"]
                         }
                     },
                     "from":(page_num-1)*NUMS_PER_PAGE,
@@ -77,6 +79,10 @@ def search(request):
     # 分页逻辑
     page = list(range(max(1,page_num-2), page_num)) +\
         list(range(page_num, min(page_num+2, MAX_PAGE)+1))
+    if page_num <= 2:
+        page = list(range(1, min(MAX_PAGE, 5)+1))
+    if page_num >= MAX_PAGE-1:
+        page = list(range(max(1, MAX_PAGE-4), MAX_PAGE+1))
     if page[0]-1 >= 2:
         page.insert(0, '...')
         page.insert(0, 1)
@@ -85,12 +91,21 @@ def search(request):
         page.append(MAX_PAGE)
         
     result_list = []
-    for result in response['hits']['hits']:
+    response = response['hits']['hits']
+    # 生成download_url的元组
+    def gen_list(url_list):
+        try:
+            url_list +=[]
+        except:
+            url_list = [url_list]
+        return url_list
+    for result in response:
         result_list.append({
+                'id': response.index(result),
                 'title': result['highlight']['title'][0] if 'title' in result['highlight'] else result['_source']['title'],
                 'type': result['highlight']['type'][0] if 'type' in result['highlight'] else result['_source']['type'],
                 'introduction': result['highlight']['introduction'][0] if 'introduction' in result['highlight'] else result['_source']['introduction'],
-                'download_url':result['_source']['download_url'],
+                'download_url':gen_list(result['_source']['download_url']),
                 'url': result['_source']['url'],
                 'age': result['_source']['age'],
                 'douban_score': result['_source']['douban_score'],
@@ -105,4 +120,5 @@ def search(request):
     context['page'] = page
     context['result_list'] = result_list
     context['search_count'] = search_count 
+    # 实现缓存，待续
     return render(request, 'result.html', context)
